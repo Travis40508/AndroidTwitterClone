79ed8456512651f48c0edf1ad6f03b777ffaee9a
Rodney Tressler <rodneytressler@Rodneys-MacBook-Pro.local>
2018-04-10 10:34:11 -0400

    [INIT] initial commit.

---
+++ app/src/main/java/com/elkcreek/rodneytressler/twitterclone/ui/MainView/MainActivity.java
@@ -0,0 +1,16 @@
+package com.elkcreek.rodneytressler.twitterclone.ui.MainView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.elkcreek.rodneytressler.twitterclone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/elkcreek/rodneytressler/twitterclone/ui/MainView/MainView.java
@@ -0,0 +1,8 @@
+package com.elkcreek.rodneytressler.twitterclone.ui.MainView;
+
+/**
+ * Created by rodneytressler on 4/10/18.
+ */
+
+public interface MainView {
+}

8346696a09c44a750e6144b8d6598ad8a6266936
Rodney Tressler <rodneytressler@Rodneys-MacBook-Pro.local>
2018-04-11 10:51:17 -0400

    [ADAPTER] adapter for posts is finished.

---
+++ app/src/main/java/com/elkcreek/rodneytressler/twitterclone/ui/Adapters/PostsAdapter.java
@@ -0,0 +1,86 @@
+package com.elkcreek.rodneytressler.twitterclone.ui.Adapters;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.elkcreek.rodneytressler.twitterclone.R;
+import com.elkcreek.rodneytressler.twitterclone.models.Post;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by rodneytressler on 4/11/18.
+ */
+
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHolder> {
+
+    private List<Post> postList;
+
+    public PostsAdapter(List<Post> postList) {
+        this.postList = postList;
+    }
+
+
+    @Override
+    public PostsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_twitter_post, parent, false);
+        return new PostsViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(PostsViewHolder holder, int position) {
+        holder.bindPosts(postList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    public class PostsViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.text_user_email)
+        protected TextView userEmail;
+
+        @BindView(R.id.text_post_content)
+        protected TextView postContent;
+
+        @BindView(R.id.text_post_time_stamp)
+        protected TextView postTimeStamp;
+
+        @BindView(R.id.image_favorite)
+        protected ImageView isFavorited;
+
+        public PostsViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindPosts(Post post) {
+            userEmail.setText(post.getEmail());
+            postContent.setText(post.getPostContent());
+            postTimeStamp.setText(post.getDate());
+            isFavorited.setImageDrawable(getFavorited(post));
+        }
+
+        private Drawable getFavorited(Post post) {
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            if(post.getLikes().contains(firebaseUser.getEmail())) {
+                return itemView.getResources().getDrawable(R.drawable.ic_favorite);
+            } else {
+                return itemView.getResources().getDrawable(R.drawable.ic_unfavorite);
+            }
+        }
+    }
+}
